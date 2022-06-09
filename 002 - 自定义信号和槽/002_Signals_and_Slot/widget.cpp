684ca33491c55b19007614c45e047791ad82d56b
NekoSilverFox <weidufox@gmail.com>
2022-06-09 14:32:59 +0300

    增加 信号连接信号再连接槽

--- 002 - 自定义信号和槽/002_Signals_and_Slot/widget.cpp
+++ 002 - 自定义信号和槽/002_Signals_and_Slot/widget.cpp
@@ -1,4 +1,5 @@
 #include "widget.h"
+#include <QPushButton>
 
 /**
  * 需求：
@@ -21,12 +22,24 @@ Widget::Widget(QWidget *parent)
 
     /* 【重点】
      * 有参的信号和槽的链接
-     * 使用函数指针，指向函数地址
+     * 使用函数指针说明参数是什么，指向函数地址
     */
     void (Teacher::*pfunc_teacher) (QString) = &Teacher::hungry;
     void (Student::*pfunc_student) (QString) = &Student::treat;
     connect(teacher, pfunc_teacher,
             student, pfunc_student);
+
+    /* 通过按钮使用信号和槽 | 信号连接信号再连接槽
+     * 一旦进行了重载就要使用函数指针，以使 connect 知道要调用哪一个函数
+     */
+    QPushButton* btn = new QPushButton("下课啦", this);
+    this->resize(600, 400);
+    void (Teacher::*pfunc_teacher_void) (void) = &Teacher::hungry;
+    void (Student::*pfunc_student_void) (void) = &Student::treat;
+    connect(btn, &QPushButton::clicked,
+            teacher, pfunc_teacher_void);  // 信号连接信号，将按钮点击连接到老师饿了
+    connect(teacher, pfunc_teacher_void,
+            student, pfunc_student_void);  // 信号连接槽，将老师饿了的信号连接至学生请客
 
 
     /* 链接之后还是不够的，要将触发老师饿了的信号 */
