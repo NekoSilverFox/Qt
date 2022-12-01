8985959cdedc59d4ba15e4a1cc2d9928d9e81a63
NekoSilverFox <weidufox@gmail.com>
2022-12-01 20:36:23 +0300

    增加 QTableWidget 控件代码及笔记

--- 013_Control_Item_Widgets/widget.cpp
+++ 013_Control_Item_Widgets/widget.cpp
@@ -1,5 +1,6 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QMessageBox>
 
 
 Widget::Widget(QWidget *parent)
@@ -56,6 +57,69 @@ Widget::Widget(QWidget *parent)
 
 
 
+    // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Table Widget @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    /* 1. 设置列数 */
+    QStringList listHead;
+    listHead << "姓名" << "性别" << "年龄";
+    ui->tableWidget->setColumnCount(listHead.size());
+
+    /* 2. 设置水平头 */
+    ui->tableWidget->setHorizontalHeaderLabels(listHead);
+
+    /* 3. 行数 */
+    ui->tableWidget->setRowCount(5);
+
+    /* 4. 设置正文 */
+    QStringList listName;
+    listName << "爱迪生" << "居里" << "特斯拉";
+
+    QList<QString> listSex;
+    listSex << "男" << "女" << "男";
+
+    for (int row = 0; row < listName.size(); row++)
+    {
+        int col = 0;
+        ui->tableWidget->setItem(row, col, new QTableWidgetItem(listName[row]));
+        col++;
+
+        ui->tableWidget->setItem(row, col, new QTableWidgetItem(listSex.at(row)));  // 【复习】.at(i) 越界会抛出异常，[i] 会直接挂掉
+        col++;
+
+        ui->tableWidget->setItem(row, col, new QTableWidgetItem(QString::number(row + 40))); // 【重点】注意 数字转 QString 的方法
+    }
+
+    /* 点击按钮添加霍金 */
+    connect(ui->btnAddHuoJin, &QPushButton::clicked,
+            this, [=](){
+        bool isEmpty = ui->tableWidget->findItems("霍金", Qt::MatchExactly).empty();  // 因为返回的是一个 List，如果没有则 List 为 empty
+
+        if (isEmpty)
+        {
+            ui->tableWidget->insertRow(0); // 线插入新的行（行号）
+            ui->tableWidget->setItem(0, 0, new QTableWidgetItem("霍金"));
+            ui->tableWidget->setItem(0, 1, new QTableWidgetItem("男"));
+            ui->tableWidget->setItem(0, 2, new QTableWidgetItem("77"));
+        }
+        else
+        {
+            QMessageBox::warning(this, "警告！！", "霍金已存在于 Table Widget");  // 【复习】标准提示框是 static 方法
+        }
+    });
+
+    /* 点击按钮删除霍金 */
+    connect(ui->btnDelHuoJin, &QPushButton::clicked,
+            this, [=](){
+        QList<QTableWidgetItem*> resFind = ui->tableWidget->findItems("霍金", Qt::MatchExactly); // findItems 返回的是一个 QList
+
+        if (resFind.empty())
+        {
+            QMessageBox::warning(this, "你找茬？", "霍金已经不在了！！！");
+        }
+        else
+        {
+            ui->tableWidget->removeRow(resFind.first()->row());  // 根据结果第一行的行号删除
+        }
+    });
 
 }
 
