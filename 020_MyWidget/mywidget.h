78b947ee377b8598932c11a15b1a66cb14cbf06f
NekoSilverFox <weidufox@gmail.com>
2022-12-03 16:54:07 +0300

    初始化 自定义控件

---
+++ 020_MyWidget/mywidget.h
@@ -0,0 +1,22 @@
+#ifndef MYWIDGET_H
+#define MYWIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class MyWidget;
+}
+
+class MyWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MyWidget(QWidget *parent = nullptr);
+    ~MyWidget();
+
+private:
+    Ui::MyWidget *ui;
+};
+
+#endif // MYWIDGET_H
