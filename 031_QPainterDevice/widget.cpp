8b571ac5d4ad9b58d893e39649ab0671e86ca3dd
NekoSilverFox <weidufox@gmail.com>
2022-12-04 19:30:32 +0300

    增加 QPixmap

--- 031_QPainterDevice/widget.cpp
+++ 031_QPainterDevice/widget.cpp
@@ -9,14 +9,19 @@ Widget::Widget(QWidget *parent)
 {
     ui->setupUi(this);
 
+    /// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ QPixmap @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     /* QPixmap 不仅仅可以作为显示图片，也可以作为绘图设备（画纸） */
     QPixmap pix(300, 300);  // 画纸大小
-    QPainter painter(&pix);  // 添加画家，并给他画纸
+    pix.fill(Qt::white);  // 设置默认填充色
 
-    painter.setPen(QPen(QColor(0, 255, 255)));
+    QPainter painter(&pix);  // 添加画家，并给他画纸
+    painter.setPen(QPen(QColor(0, 255, 0)));
     painter.drawEllipse(QPoint(150, 150), 100, 100);
 
-    pix.save("res_pix.png");  // 保存到磁盘
+    pix.save("/Users/fox/雪狸的文件/Programma/Qt/031_QPainterDevice/res_pix.png");  // 保存到磁盘
+
+
+
 }
 
 Widget::~Widget()
