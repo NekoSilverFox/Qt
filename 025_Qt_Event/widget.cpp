885769d8a75b06e9e9aaa2362fd265dcd8381bfe
NekoSilverFox <weidufox@gmail.com>
2022-12-04 15:38:15 +0300

    增加 定时器的第二种方式

--- 025_Qt_Event/widget.cpp
+++ 025_Qt_Event/widget.cpp
@@ -1,6 +1,7 @@
 #include "widget.h"
 #include "ui_widget.h"
 #include <QTimerEvent>
+#include <QTimer>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -11,6 +12,20 @@ Widget::Widget(QWidget *parent)
     /* 启动定时器 */
     timer_id1 = this->startTimer(1000);  // 每 1000ms（1s），调用一次 timeEvent 事件
     timer_id2 = this->startTimer(2000);
+
+    /* 定时器第二种方式（推荐） */
+    QTimer* timer3 = new QTimer(this);
+    timer3->start(500);  // 启动定时器
+    connect(timer3, &QTimer::timeout,
+            this, [=](){
+        static int num3 = 0;
+        ui->label_4->setText(QString::number(num3++));
+    });
+
+    /* 点击按钮暂停 */
+     connect(ui->btnStop, &QPushButton::clicked,
+             this, [=](){timer3->stop();});
+
 
 }
 
